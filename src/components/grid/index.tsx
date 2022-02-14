031103cdd107df23e694f5787fce118ec5bf0d99
Grzegorz Celek <g.celek@gmail.com>
2022-02-14 15:22:15 +0100

    add redux

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,13 +1,18 @@
-import React, { FC, Children } from 'react'
-
-import { createFullGrid } from 'utils'
+import React, { FC, Children, useEffect, useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { Dispatch, AnyAction } from 'redux'
 
 import { Container, Row } from './styles'
 import Block from './block'
+import { createGrid } from 'reducers'
+import { INDEX } from 'typings'
 
 const Grid: FC = () => {
-  const grid = createFullGrid()
-  console.log(grid)
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+  const create = useCallback(() => dispatch(createGrid()), [dispatch])
+  useEffect(() => {
+    create()
+  }, [create])
 
   return (
     <Container data-cy="grid-container">
@@ -16,7 +21,10 @@ const Grid: FC = () => {
           <Row data-cy="grid-row-container">
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <Block colIndex={colIndex} rowIndex={rowIndex} />
+                <Block
+                  colIndex={colIndex as INDEX}
+                  rowIndex={rowIndex as INDEX}
+                />
               ))
             )}
           </Row>
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
 
 import { Content, Title, Grid, Card } from 'components'
 import reportWebVitals from 'core/reportWebVitals'
-import { unregister } from 'core'
+import { configureStore, unregister } from 'core'
 import { GlobalStyles, theme } from 'styles'
+
+const store = configureStore()
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content data-cy="content">
-      <Title data-cy="title">Sudoku</Title>
-      <Card data-cy="card">
-        <Grid />
-      </Card>
-    </Content>
+    <Provider store={store}>
+      <Content data-cy="content">
+        <Title data-cy="title">Sudoku</Title>
+        <Card data-cy="card">
+          <Grid />
+        </Card>
+      </Content>
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 )
