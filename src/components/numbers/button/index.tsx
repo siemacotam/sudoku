631570d1321318b9452815f8e35f273e087e5b43
Grzegorz Celek <g.celek@gmail.com>
2022-02-15 12:30:22 +0100

    add functionality to number button

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,16 +1,38 @@
-import { FC } from 'react'
-import { NUMBERS } from 'typings'
+import { FC, useCallback } from 'react'
+import { BLOCK_COORDS, N, NUMBERS } from 'typings'
 
 import { Button } from 'components'
+import { useDispatch, useSelector } from 'react-redux'
+import { fillBlock, IReducer } from 'reducers'
+import { AnyAction, Dispatch } from 'redux'
 
 interface IProps {
   value: NUMBERS
 }
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS
+  selectedValue: N
+}
+
 const NumberButton: FC<IProps> = ({ value }) => {
-  function fill() {
-    console.log('fill', value)
-  }
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  )
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+  const fill = useCallback(() => {
+    if (state.selectedBlock && state.selectedValue === 0) {
+      dispatch(fillBlock(value, state.selectedBlock))
+    }
+  }, [dispatch, state.selectedBlock, state.selectedValue, value])
+
   return <Button onClick={fill}>{value}</Button>
 }
 
